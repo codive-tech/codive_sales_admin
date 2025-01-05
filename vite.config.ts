4a967e997d6960682a40dfae9bde11ea82e31c9f
Gunashekar0619 <gunashekar.c.619@gmail.com>
2025-01-05 19:42:27 +0530

    deploy port change

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  preview: {
+    port: 3004,
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
