1ca3890b96180d2103aa629cd126634c245bf54a
Gunashekar0619 <57127076+Gunashekar0619@users.noreply.github.com>
2025-07-07 18:41:35 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   preview: {
-    port: 3004,
+    port: 4174,
   },
   plugins: [react()],
   optimizeDeps: {
