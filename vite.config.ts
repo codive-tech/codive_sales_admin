22642f11046e766d5b451ca4a1986124a846401e
codive-tech <codivetech@gmail.com>
2025-07-08 09:37:42 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   preview: {
     port: 3004,
+    allowedHosts: ['sales.admin.codive.co', 'partner.codive.co', 'www.partner.codive.co']
   },
   plugins: [react()],
   optimizeDeps: {
